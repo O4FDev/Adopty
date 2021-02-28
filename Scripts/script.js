da07e521edd76b71de48d91c269b5c116a0b9894
O4FDev <lukelucasdiscord@gmail.com>
2021-02-28 00:10:51 +0000

    Java Script is fun

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+var button = document.getElementById('button');
+
+button.style.cursor = 'pointer';
+button.onclick = function() {
+    window.location.replace("http://www.instagram.com/hobbing._");
+};
+
