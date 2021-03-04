b84b77f409e5f44eab6cef7c4b752ef115dee4fc
Luke L <52675277+O4FDev@users.noreply.github.com>
2021-03-04 11:52:55 +0000

    Removed personal information for universities to be allowed to use it

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,6 +2,6 @@ var button = document.getElementById('button');
 
 button.style.cursor = 'pointer';
 button.onclick = function() {
-    window.location.replace("http://www.instagram.com/hobbing._");
+    window.location.replace("http://www.instagram.com/");
 };
 
